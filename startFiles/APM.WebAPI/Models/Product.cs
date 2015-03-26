70bb08fda9d688de798dd4cfcd546b712a104b8e
DeborahK <deborahk@insteptech.com>
2015-03-26 15:11:21 -0700

    Added Web API Start Files

---
+++ startFiles/APM.WebAPI/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APM.WebAPI.Models
+{
+    public class Product
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ProductCode { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
---
+++ startFiles/APM.WebAPI/Models/ProductRepository.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Hosting;
+
+namespace APM.WebAPI.Models
+{
+    /// <summary>
+    /// Stores the data in a json file so that no database is required for this
+    /// sample application
+    /// </summary>
+    public class ProductRepository
+    {
+        /// <summary>
+        /// Creates a new product with default values
+        /// </summary>
+        /// <returns></returns>
+        internal Product Create()
+        {
+            Product product = new Product
+            {
+                ReleaseDate = DateTime.Now
+            };
+            return product;
+        }
+
+        /// <summary>
+        /// Retrieves the list of products.
+        /// </summary>
+        /// <returns></returns>
+        internal List<Product> Retrieve()
+        {
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+
+            var json = System.IO.File.ReadAllText(filePath);
+
+            var products = JsonConvert.DeserializeObject<List<Product>>(json);
+
+            return products;
+        }
+
+        /// <summary>
+        /// Saves a new product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        internal Product Save(Product product)
+        {
+            // Read in the existing products
+            var products = this.Retrieve();
+
+            // Assign a new Id
+            var maxId = products.Max(p => p.ProductId);
+            product.ProductId = maxId + 1;
+            products.Add(product);
+
+            WriteData(products);
+            return product;
+        }
+
+        /// <summary>
+        /// Updates an existing product
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        internal Product Save(int id, Product product)
+        {
+            // Read in the existing products
+            var products = this.Retrieve();
+
+            // Locate and replace the item
+            var itemIndex = products.FindIndex(p => p.ProductId == product.ProductId);
+            if (itemIndex > 0)
+            {
+                products[itemIndex] = product;
+            }
+            else
+            {
+                return null;
+            }
+
+            WriteData(products);
+            return product;
+
+
+        private bool WriteData(List<Product> products)
+        {
+            // Write out the Json
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+
+            var json = JsonConvert.SerializeObject(products, Formatting.Indented);
+            System.IO.File.WriteAllText(filePath, json);
+
+            return true;
+        }
+
+    }
+}
